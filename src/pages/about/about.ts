bf8ab53cd91ab823ca84e18f1276a66833abe46c
Donald Ng <donaldyann@gmail.com>
2017-10-28 02:25:26 +0800

    Added mockup for expenses

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,9 +6,19 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-
   constructor(public navCtrl: NavController) {
 
   }
+
+
+  expensesList = [
+      {name: 'Animals', body: Math.floor((Math.random() * 10) + 1) },
+      {name: 'Body Parts', body: Math.floor((Math.random() * 10) + 1) },
+      {name: 'Colors & Shapes', body: Math.floor((Math.random() * 10) + 1) },
+      {name: 'Apparels', body: Math.floor((Math.random() * 10) + 1) },
+      {name: 'Family', body: Math.floor((Math.random() * 10) + 1) },
+      {name: 'Vegitables & Fruits', body: Math.floor((Math.random() * 10) + 1) }
+
+  ];
 
 }
