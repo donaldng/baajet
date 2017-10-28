f18e7d8aff56d024376f132489e345c6e86219e0
Donald Ng <donaldyann@gmail.com>
2017-10-28 11:49:49 +0800

    Added new components in UI

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,23 +1,59 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ActionSheetController } from 'ionic-angular'
+import { ModalController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public actionSheetCtrl: ActionSheetController, public modalCtrl: ModalController) {
 
   }
 
+ presentActionSheet(theItem) {
+   const actionSheet = this.actionSheetCtrl.create({
+     title: 'Action',
+     buttons: [
+       {
+         text: 'Delete',
+         role: 'destructive',
+         handler: () => {
+            let index = this.expensesList.indexOf(theItem);
+            this.expensesList.splice(index,1);
+         }
+       },
+       {
+         text: 'Edit',
+         handler: () => {
+         }
+       },
+       {
+         text: 'Cancel',
+         role: 'cancel',
+         handler: () => {
+         }
+       }
+     ]
+   });
+
+   actionSheet.present();
+ }
+
+ presentProfileModal() {
+   const profileModal = this.modalCtrl.create(Profile, { userId: 8675309 });
+   profileModal.present();
+ }
+
 
   expensesList = [
-      {name: 'Animals', body: Math.floor((Math.random() * 10) + 1) },
-      {name: 'Body Parts', body: Math.floor((Math.random() * 10) + 1) },
-      {name: 'Colors & Shapes', body: Math.floor((Math.random() * 10) + 1) },
-      {name: 'Apparels', body: Math.floor((Math.random() * 10) + 1) },
-      {name: 'Family', body: Math.floor((Math.random() * 10) + 1) },
-      {name: 'Vegitables & Fruits', body: Math.floor((Math.random() * 10) + 1) }
+      {id: 1, name: 'Animals', datetime: '12th Sept', frequency: 'Once', body: Math.floor((Math.random() * 10) + 1) },
+      {id: 2, name: 'Body Parts', datetime: '12th Sept', frequency: 'Monthly', body: Math.floor((Math.random() * 10) + 1) },
+      {id: 3, name: 'Colors & Shapes', datetime: '12th Sept', frequency: 'Once', body: Math.floor((Math.random() * 10) + 1) },
+      {id: 4, name: 'Apparels', datetime: '12th Sept', frequency: 'Daily', body: Math.floor((Math.random() * 10) + 1) },
+      {id: 5, name: 'Family', datetime: '12th Sept', frequency: 'Once', body: Math.floor((Math.random() * 10) + 1) },
+      {id: 6, name: 'Vegitables & Fruits', datetime: '12th Sept', frequency: 'Weekly', body: Math.floor((Math.random() * 10) + 1) }
 
   ];
 
