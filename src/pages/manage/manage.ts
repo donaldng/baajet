9d5e941e579d964dc8580475aababca71144ba08
Donald Ng <donaldyann@gmail.com>
2017-11-03 22:36:06 +0800

    Fixed bug - using wrong function to call camera native

--- src/pages/manage/manage.ts
+++ src/pages/manage/manage.ts
@@ -75,24 +75,14 @@ export class ManagePage {
         return -1;
     }
     chooseImage(){
-        const options: CameraOptions = {
-            quality: 100,
-            targetWidth: 1000,
-            targetHeight: 1000,
-            destinationType: this.camera.DestinationType.DATA_URL,
-            encodingType: this.camera.EncodingType.PNG,
-            sourceType: this.camera.SourceType.PHOTOLIBRARY,
-            mediaType: this.camera.MediaType.ALLMEDIA,
-            correctOrientation: true
-        }
-
-        this.camera.getPicture(options).then((imageData) => {
-            // imageData is either a base64 encoded string or a file URI
-            // If it's base64:
-            this.tmpImage = 'data:image/jpeg;base64,' + imageData;
-        }, (err) => {
-            // Handle error
-        });        
+            var options = {
+                sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+                destinationType: this.camera.DestinationType.FILE_URI
+            };
+            this.camera.getPicture(options).then((imageData) => {
+                this.tmpImage = imageData;
+            }, (err) => {
+            });
     }
     captureImage(){
         const options: CameraOptions = {
