47c953a40a2385bb9f0b6607eb337f427098b4d7
Donald Ng <donaldyann@gmail.com>
2017-11-18 17:01:14 +0800

    Claim features

--- src/pages/expenses/expenses.ts
+++ src/pages/expenses/expenses.ts
@@ -239,15 +239,26 @@ export class ExpensesPage {
         return 'recurring';
     }
 
-    quickAdd(){
+    quickAdd(claim){
+        let title = 'Add expenses';
+        let placeholder = '';
+        let value = '';
+
+        if (claim){
+            title = "Claim reserved fund";
+            placeholder = claim.amount;
+            value = claim.amount;
+        }
+
         let prompt = this.alertCtrl.create({
-            title: 'Add expenses',
+            title: title,
             message: "How much is the expenses?",
             inputs: [
             {
                 name: 'price',
-                placeholder: '',
-                type: 'number'
+                placeholder: placeholder,
+                type: 'number',
+                value: value
             },
             ],
             buttons: [
@@ -259,8 +270,14 @@ export class ExpensesPage {
             {
                 text: 'Go',
                 handler: data => {
-                    this.init_price = data.price;
-                    this.gotoManage('-1');
+                    if(!claim){
+                        this.init_price = data.price;
+                        this.gotoManage('-1');
+                    }
+                    else{
+                        // If claim direct add one
+                        this.claimExpenses(claim, data.price);
+                    }
                 }
             }
             ]
@@ -270,6 +287,65 @@ export class ExpensesPage {
             firstInput.focus();
             return;
         });        
+    }
+
+    claimExpenses(expenses, price){
+        let index = this.findIndex(expenses.id);
+
+        // Validate
+        if (price > expenses.amount){
+            alert('You cannot claim more than your reserved fund, my good sir!');
+            return;
+        }
+        else if (price < 0){
+            alert('I cannot process negative number, baaaaa...');
+            return;
+        }
+
+        // Remove amount from claim
+        expenses.amount = expenses.amount - price;
+        this.expensesList[index] = expenses;
+
+        let tmpImage = 0;
+        if (typeof expenses.tmpImage != 'undefined') tmpImage = expenses.tmpImage;
+
+        let thumbnail = 0;
+        if (typeof expenses.thumbnail != 'undefined') thumbnail = expenses.thumbnail;
+
+        // Add new expenses
+        var newExpenses = {
+            'id': Math.round((new Date()).getTime() / 1000),
+            'name': expenses.name,
+            'amount': Number(price),
+            'freq': 0,
+            'freq_start': new Date().toISOString().slice(0, 19).replace('T',' '),
+            'freq_end': new Date().toISOString().slice(0, 19).replace('T',' '),
+            'datetime': new Date().toISOString().slice(0, 19).replace('T',' '),
+            'image': tmpImage,
+            'thumbnail': thumbnail,
+            'todays': true
+        };
+        this.expensesList.push(newExpenses);
+
+        // Delete claim if depleted
+        if (expenses.amount <= 0){
+            let index = this.expensesList.indexOf(expenses);
+            this.expensesList.splice(index,1);
+        }
+
+        this.events.publish('reload:home','expensesList',this.expensesList);
+        this.events.publish('change_segment', newExpenses.freq);
+        this.storage.set('expensesList', this.expensesList);
+        this.events.publish('reload:expenses', this.expensesList);
+    }
+
+    findIndex(find_id){
+        for (var i = 0, len = this.expensesList.length; i < len; i++) {
+            if (this.expensesList[i].id == find_id){
+                return i;
+            }
+        }
+        return -1;
     }
 
     findRunningId(){
@@ -291,6 +367,44 @@ export class ExpensesPage {
 
         this.runningId = Number(return_id) + 1;        
     }  
+
+    notReserved(expenses){
+        if(expenses.freq != 1){
+            this.gotoManage(expenses.id);
+        }
+        else{
+            this.presentClaimOption(expenses);
+        }
+    }
+
+    presentClaimOption(expenses){
+        const actionSheet = this.actionSheetCtrl.create({
+            title: 'Action',
+            buttons: [
+            {
+                text: 'Edit',
+                handler: () => {
+                    this.gotoManage(expenses.id);
+                }
+            },
+            {
+                text: 'Claim',
+                handler: () => {
+                    let claim = expenses;
+                    this.quickAdd(claim);
+                }
+            },
+            {
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => {
+                }
+            }
+            ]
+        });
+
+        actionSheet.present();
+    }
 
     getDefaultThumbnail(name, type){
         
